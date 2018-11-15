90c642edd26a4ace60acfb7a962f2133b9f89722
Peter Mnev <petermnev@gmail.com>
2018-11-15 17:48:55 -0500

    Added file iteration loop

--- catkin_ws/src/imaging_downlink/scripts/image_sender.py
+++ catkin_ws/src/imaging_downlink/scripts/image_sender.py
@@ -9,11 +9,13 @@ from imaging_downlink.msg import uav_image_Msg
 import cv2
 import numpy as np
 import time
+import os
 import sys
 from cv_bridge import CvBridge, CvBridgeError
 
 
 booley = True
+sentfiles = []
 
 def confirm(nothing):
 	print('confirmed')
@@ -25,21 +27,36 @@ def image_sender():
 	rospy.Subscriber('ImageConfirm', Empty, confirm)	
 	pub = rospy.Publisher('ImageSender', uav_image_Msg, queue_size=1)
 	rospy.init_node('image_sender', anonymous=True)	
-	bridge = CvBridge()
-	img = cv2.imread('/home/imaging/catkin_ws/src/imaging_downlink/scripts/trees.jpeg', 1)
-	output_im = bridge.cv2_to_imgmsg(img, encoding="bgr8")	
-
-	pois = Point(1,2,3)
-
-	message = uav_image_Msg()
-	message.image = output_im
-	message.pos = pois
-
-	rospy.loginfo(message)
-	booley = False	
-	pub.publish(message) 
 	
-	rospy.spin()
+
+
+
+	while True:
+		
+		file_list = os.listdir("/home/imaging/PlanePics")	
+		for filey in file_list:
+			if filey not in sentfiles:
+
+				bridge = CvBridge()
+				img = cv2.imread('/home/imaging/PlanePics/'+filey, 1)
+				output_im = bridge.cv2_to_imgmsg(img, encoding="bgr8")	
+
+				pois = Point(1,2,3)
+
+				message = uav_image_Msg()
+				message.image = output_im
+				message.pos = pois
+
+				rospy.loginfo(message)
+				booley = False	
+				pub.publish(message) 
+		
+				while booley is False:
+					time.wait(50)		
+	
+				
+		
+	
 
 
 if __name__ == '__main__':
--- catkin_ws/src/imaging_downlink/scripts/imaging_recv.py
+++ catkin_ws/src/imaging_downlink/scripts/imaging_recv.py
@@ -13,15 +13,17 @@ from cv_bridge import CvBridge, CvBridgeError
 
 
 pub = rospy.Publisher('ImageConfirm', Empty, queue_size=1)	
+count = 1
+
 
 def image_receiver(data):
 	bridge = CvBridge()
 
 	try:
 		cv_image = bridge.imgmsg_to_cv2(data.image, "bgr8")
-		cv2.imwrite('gottem.jpeg', cv_image)
-		cv2.imwrite('/home/imaging/catkin_ws/src/imaging_downlink/scripts/gottem.jpeg',  cv_image )
+		cv2.imwrite('/home/imaging/GroundPics/Picture.jpeg',  cv_image )
 		pub.publish()
+		
 
 
 	except CvBridgeError as e:
