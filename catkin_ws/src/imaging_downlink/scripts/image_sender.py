0c75f38f743fa18450e81350d4db6fc2d05f061f
Peter Mnev <petermnev@gmail.com>
2018-11-30 13:31:26 -0500

    Added Search Loop, fixed not working loop

--- catkin_ws/src/imaging_downlink/scripts/image_sender.py
+++ catkin_ws/src/imaging_downlink/scripts/image_sender.py
@@ -11,54 +11,77 @@ import numpy as np
 import time
 import os
 import sys
+import signal
 from cv_bridge import CvBridge, CvBridgeError
 
 
-booley = True
-sentfiles = []
+def sigterm_handler(signal, frame):
+    # save the state here or do whatever you want
+    print('booyah! bye bye')
+    sys.exit(1)
 
-def confirm(nothing):
-	print('confirmed')
-	booley = True
+signal.signal(signal.SIGINT, sigterm_handler)
+
+
+
+
+class image_sender:
+
+
+	def confirm(self, Empty):
+		print('confirmed')
+		self.booley = True
+
+
+
 
 	
 
-def image_sender():
-	rospy.Subscriber('ImageConfirm', Empty, confirm)	
-	pub = rospy.Publisher('ImageSender', uav_image_Msg, queue_size=1)
-	rospy.init_node('image_sender', anonymous=True)	
+	def image_sender_method(self):
+
+	
 	
 
 
 
-	while True:
-		
-		file_list = os.listdir("/home/imaging/PlanePics")	
-		for filey in file_list:
-			if filey not in sentfiles:
+		while True:
+			time.sleep(.05)	
+			file_list = os.listdir("/home/imaging/PlanePics")		
+			for filey in file_list:
+				if filey not in self.sentfiles:
+					print filey
+					self.bridge = CvBridge()
+					self.img = cv2.imread('/home/imaging/PlanePics/'+filey, 1)
+					output_im = self.bridge.cv2_to_imgmsg(self.img, encoding="bgr8")
+					self.sentfiles.append(filey)	
 
-				bridge = CvBridge()
-				img = cv2.imread('/home/imaging/PlanePics/'+filey, 1)
-				output_im = bridge.cv2_to_imgmsg(img, encoding="bgr8")	
+					pois = Point(1,2,3)
 
-				pois = Point(1,2,3)
+					message = uav_image_Msg()
+					message.image = output_im
+					message.pos = pois
 
-				message = uav_image_Msg()
-				message.image = output_im
-				message.pos = pois
-
-				rospy.loginfo(message)
-				booley = False	
-				pub.publish(message) 
-		
-				while booley is False:
-					time.wait(50)		
+					rospy.loginfo(message)
+					self.booley = False	
+					self.pub.publish(message) 
+					while self.booley == False:
+						time.sleep(.05)
+			
 	
 				
 		
 	
 
 
-if __name__ == '__main__':
-	image_sender()
+	def __init__(self):
+		
+		rospy.init_node('image_sender_thing', anonymous=True)
+		rospy.Subscriber('ImageConfirm', Empty, self.confirm)	
+		self.pub = rospy.Publisher('ImageSender', uav_image_Msg, queue_size=1)
+		self.booley = True
+		self.sentfiles = []
+		self.image_sender_method()
+	
+
+image_sender()
 
